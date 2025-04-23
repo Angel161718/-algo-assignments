1b7e44b6922d384bff8de48c42fbd5a4aedf2c7c
Angel161718 <101666842+Angel161718@users.noreply.github.com>
2025-04-24 01:51:19 +0300

    transposition_graphs.py

--- assignment-2025-1/transposition_graphs.py
+++ assignment-2025-1/transposition_graphs.py
@@ -1,5 +1,12 @@
 from itertools import permutations
 import argparse
+def generate_permutations(s, t):
+    base = ['0'] * s + ['1'] * t
+    perms = set(permutations(base))  # μοναδικές μεταθέσεις
+    bin_strs = [''.join(p) for p in perms]
+    index_reprs = [[i for i, bit in enumerate(b) if bit == '1'] for b in bin_strs]
+    int_vals = [int(b, 2) for b in bin_strs]
+    return bin_strs, index_reprs, int_vals
 
 def main():
     parser = argparse.ArgumentParser(description="Γράφος μεταθέσεων με DFS και BTS")
