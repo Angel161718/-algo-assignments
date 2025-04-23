af03d97e1afaece293b62cdc50bff7f94ada1837
Angel161718 <101666842+Angel161718@users.noreply.github.com>
2025-04-24 00:22:10 +0300

    transposition_graphs.py

--- assignment-2025-1/transposition_graphs.py
+++ assignment-2025-1/transposition_graphs.py
@@ -1,3 +1,6 @@
+from itertools import permutations
+import argparse
+
 def main():
     parser = argparse.ArgumentParser(description="Γράφος μεταθέσεων με DFS και BTS")
     parser.add_argument("s", type=int, help="Αριθμός μηδενικών")
