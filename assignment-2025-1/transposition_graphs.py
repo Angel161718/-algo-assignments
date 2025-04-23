a5d5764cb67cc0332610b9f739cbdd3bf0ef4c22
Angel161718 <101666842+Angel161718@users.noreply.github.com>
2025-04-24 00:21:01 +0300

    transposition_graphs.py

--- assignment-2025-1/transposition_graphs.py
+++ assignment-2025-1/transposition_graphs.py
@@ -1,1 +1,7 @@
-
+def main():
+    parser = argparse.ArgumentParser(description="Γράφος μεταθέσεων με DFS και BTS")
+    parser.add_argument("s", type=int, help="Αριθμός μηδενικών")
+    parser.add_argument("t", type=int, help="Αριθμός άσσων")
+    parser.add_argument("mode", choices=["graph", "dfs", "bts"], help="Λειτουργία: graph, dfs ή bts")
+    args = parser.parse_args()
+  
