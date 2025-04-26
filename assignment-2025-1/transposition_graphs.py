edf734ab2ad920e74e5cd423974d5c281182ddff
Angel161718 <101666842+Angel161718@users.noreply.github.com>
2025-04-26 03:04:27 +0300

    transposition_graphs.py

--- assignment-2025-1/transposition_graphs.py
+++ assignment-2025-1/transposition_graphs.py
@@ -27,8 +27,10 @@ def is_genlex_path(index_paths):
         if prefix != next_prefix:
             return False
     return True
+    
 def hamming_index_distance(a, b):
     return sum(x != y for x, y in zip(a, b))
+    
 def dfs_search(path, visited, graph, idx_map, all_paths):
     if len(visited) == len(graph):
         all_paths.append(path[:])
@@ -41,13 +43,65 @@ def dfs_search(path, visited, graph, idx_map, all_paths):
                 visited.add(neighbor)
                 dfs_search(new_path, visited, graph, idx_map, all_paths)
                 visited.remove(neighbor)
+                
+def find_all_dfs_paths(graph, idx_map):
+    all_paths = []
+    for start in graph:
+        dfs_search([start], {start}, graph, idx_map, all_paths)
+    return all_paths
 
+def generate_sigma_strings(s, t):
+    total = 2 ** (s - 1)
+    sigmas = []
+    for i in range(total):
+        bits = bin(i)[2:].zfill(s - 1)
+        suffix = ''.join('-' if b == '1' else '+' for b in bits)
+        sigmas.append('0' * t + '-' + suffix)
+    return sigmas
+    
+def next_bts_state(state):
+    state = list(state)
+    n = len(state)
+    for i in range(n - 2, -1, -1):
+        if i >= 1 and state[i - 1] == '0' and state[i] == '-' and state[i + 1] == '+':
+            state[i - 1:i + 2] = ['-', '+', '0']
+            for j in range(i + 2, n):
+                if state[j] in '+-':
+                    state[j] = {'+': '-', '-': '+'}[state[j]]
+                    break
+            return ''.join(state)
+        elif i >= 1 and state[i] == '+' and state[i + 1] == '-':
+            state[i:i + 2] = ['0', '+']
+            for j in range(i + 2, n):
+                if state[j] in '+-':
+                    state[j] = {'+': '-', '-': '+'}[state[j]]
+                    break
+            return ''.join(state)
+    return None
+
+def bts_to_binary(seq):
+    return ''.join('1' if c == '0' else '0' for c in seq)
+
+def bts_generate_paths(s, t):
+    paths = []
+    for sigma in generate_sigma_strings(s, t):
+        current = sigma
+        path = [current]
+        while True:
+            next_state = next_bts_state(current)
+            if not next_state or next_state in path:
+                break
+            path.append(next_state)
+            current = next_state
+        paths.append(path)
+    return paths
 
 def find_all_dfs_paths(graph, idx_map):
     all_paths = []
     for start in graph:
         dfs_search([start], {start}, graph, idx_map, all_paths)
     return all_paths
+
 def generate_sigma_strings(s, t):
     total = 2 ** (s - 1)
     sigmas = []
